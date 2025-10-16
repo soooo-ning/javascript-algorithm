846c10e5f2422ceb18b6127c465d3580568296ca
Sonnie <ssoning13@gmail.com>
2025-10-16 13:45:49 +0000

    test

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+function sum(a, b, ...rest) {
+  console.log(a, b, rest) // 1 2 [3, 4, 5, 6, 7, 8]
+  return rest.reduce((acc, cur) => acc + cur, 0)
+}
+const res = sum(1, 2, 3, 4, 5, 6, 7, 8)
+console.log(res) // 33
